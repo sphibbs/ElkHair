133f6ba04b0cf8649eb9d91bd13a7131c77d47a1
Scott Phibbs <scottph@microsoft.com>
2020-10-14 13:17:31 -0700

    initial commit

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace NetCoreTelemetryTest
+{
+    class Class1
+    {
+        //I wonder, how much typing it thinks I am doing
+        // Hmm, 
+        int x = 1;
+        Return x;
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NetCoreTelemetryTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("testing the editor keystroke telemetry");
+            //typing
+        }
+    }
+}
